2c838abddea3012a1c4b37efb42ccb5c68088ac4
Leigh Murray <leighmurray@gmail.com>
2022-05-05 00:36:40 +0200

    added test device code

---
+++ testdevice/testdevice.ino
@@ -0,0 +1,71 @@
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioInputI2S            inputDeviceI2S; //xy=188,142
+AudioRecordQueue         inputBuffer;    //xy=377,138
+AudioPlayQueue           outputBuffer;   //xy=525,134
+AudioOutputI2S           outputDeviceI2S; //xy=732,141
+AudioConnection          patchCord1(inputDeviceI2S, 0, inputBuffer, 0);
+AudioConnection          patchCord2(outputBuffer, 0, outputDeviceI2S, 0);
+AudioControlSGTL5000     sgtl5000_1;     //xy=438,270
+// GUItool: end automatically generated code
+
+const int myInput = AUDIO_INPUT_LINEIN;
+
+bool outputVoltage = LOW;
+int outputVoltagePin = 24;
+IntervalTimer toggleVoltageTimer;
+
+void toggleOutputVoltage(){
+  setOutputVoltage(!outputVoltage);
+}
+
+void setOutputVoltage(bool newOutputVoltage){
+  outputVoltage = newOutputVoltage;
+  digitalWriteFast(outputVoltagePin, outputVoltage);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  AudioMemory(128);
+  sgtl5000_1.enable();
+  sgtl5000_1.inputSelect(myInput);
+  sgtl5000_1.volume(1.0);
+  pinMode(outputVoltagePin, OUTPUT);
+  digitalWriteFast(outputVoltagePin, outputVoltage);
+}
+
+void testVoltage(){
+  uint16_t spikeSampled = false;
+  uint16_t latencyDuration = 0;
+  digitalWriteFast(outputVoltagePin, HIGH);
+  elapsedMicros latencyMicros;
+  inputBuffer.begin();  
+  while(!spikeSampled){
+    while (inputBuffer.available() >= 1){
+      latencyDuration = latencyMicros;
+      int16_t leftChannelSamples[AUDIO_BLOCK_SAMPLES];
+      memcpy(leftChannelSamples, inputBuffer.readBuffer(), AUDIO_BLOCK_SAMPLES*2);
+      inputBuffer.freeBuffer();
+      for (int i=0; i<AUDIO_BLOCK_SAMPLES; i++){
+        if (leftChannelSamples[i] < -10000){
+          spikeSampled = true;
+          break;
+        }
+      }
+    }
+  }
+  Serial.println("Sample found after: " + String(latencyDuration) + " microseconds");
+  digitalWriteFast(outputVoltagePin, LOW);
+  inputBuffer.end();
+  inputBuffer.clear();
+}
+
+void loop() {
+  delay(2000);
+  testVoltage();
+}
