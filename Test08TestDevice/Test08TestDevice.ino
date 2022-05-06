b58661563a898ed8a961b11d58f81105d03305ec
Leigh Murray <leighmurray@gmail.com>
2022-05-06 17:09:55 +0200

    more tests

--- Test08TestDevice/Test08TestDevice.ino
+++ Test08TestDevice/Test08TestDevice.ino
@@ -3,16 +3,20 @@
 
 using namespace qindesign::network;
 
-const int numberOfPackets = 100000;
+const uint8_t numberOfTests = 100;
+const int testInterval = 1000;
 
 constexpr uint32_t kDHCPTimeout = 10000;  // 10 seconds
 constexpr uint16_t dataPort = 4464;
 
 EthernetUDP udp;
 
+uint8_t buf[Ethernet.mtu() - 20 - 8];  // Maximum UDP payload size
+                                       // 20-byte IP, 8-byte UDP header
+
 uint8_t mac[6];
 
-const int testPacketSize = Ethernet.mtu() - 20 - 8;
+const int testPacketSize = 8;
 uint8_t dataPacket[testPacketSize];
 
 IPAddress remoteIP;
@@ -34,6 +38,9 @@ void setupNetwork () {
   Serial.println(linkEstablished);
   
   remoteIP = clientIP;
+  
+  // Listen on port
+  udp.begin(dataPort);
 }
 
 void setup() {
@@ -44,15 +51,30 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("Press ENTER to begin: ");
-  while(!Serial.available()) {
-    Ethernet.loop();
-  }
+  Serial.print("Press ENTER to begin testing packet size " + String(testPacketSize) + " bytes: ");
+  while(!Serial.available());
   Serial.read();
   Serial.println("Starting!");
-  for (int i=0; i<numberOfPackets; i++) {
-    udp.send(remoteIP, dataPort, dataPacket, testPacketSize);
+  for (int j=0; j<100; j++) {
     Ethernet.loop();
+  }
+  for (uint8_t i=0; i<numberOfTests; i++) {
+    uint16_t storedLatency = 0;
+    elapsedMicros ethernetDelay;
+    dataPacket[0] = i;
+    udp.send(remoteIP, dataPort, dataPacket, testPacketSize);
+    while(storedLatency == 0){
+      uint16_t size = udp.parsePacket();
+      if (0 < size && size <= sizeof(buf)) {
+        storedLatency = ethernetDelay;
+        udp.read(buf, size);
+        if (buf[0] != i) {
+          Serial.println("Incorrect packet received");
+        }
+      }
+    }
+    Serial.print(String(storedLatency) + ",");
+    delay(testInterval);
   }
   Serial.println("\nDone!");
 }
